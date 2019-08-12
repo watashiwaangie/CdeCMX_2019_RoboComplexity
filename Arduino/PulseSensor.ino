0e42a9b1ce0f91a787448f1d94d3b1883d5d426e
Angeles Teresa Mena Dzul <49109494+watashiwaangie@users.noreply.github.com>
2019-08-12 15:07:51 -0500

    Update PulseSensor.ino

--- Arduino/PulseSensor.ino
+++ Arduino/PulseSensor.ino
@@ -1,25 +1,3 @@
-
-/*  PulseSensor Starter Project and Signal Tester
- *  The Best Way to Get Started  With, or See the Raw Signal of, your PulseSensor.com™ & Arduino.
- *
- *  Here is a link to the tutorial
- *  https://pulsesensor.com/pages/code-and-guide
- *
- *  WATCH ME (Tutorial Video):
- *  https://www.youtube.com/watch?v=RbB8NSRa5X4
- *
- *
--------------------------------------------------------------
-1) This shows a live human Heartbeat Pulse.
-2) Live visualization in Arduino's Cool "Serial Plotter".
-3) Blink an LED on each Heartbeat.
-4) This is the direct Pulse Sensor's Signal.
-5) A great first-step in troubleshooting your circuit and connections.
-6) "Human-readable" code that is newbie friendly."
-
-*/
-
-
 //  Variables
 int PulseSensorPurplePin = 0;        // Pulse Sensor PURPLE WIRE connected to ANALOG PIN 0
 int LED13 = 13;   //  The on-board Arduion LED
